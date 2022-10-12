d4404347494bf8b33858061d9f6f7a1a71a49c83
MohamedElmdary <engm5081@gmail.com>
2022-10-12 12:26:03 +0200

    Add abstract_control in internals

---
+++ src/internals/abstract_control.ts
@@ -0,0 +1,26 @@
+import RxStore from "./rx_store";
+
+enum Status {
+    valid = "VALID",
+    invalid = "INVALID",
+    pending = "PENDING",
+}
+
+abstract class AbstractControl<T> extends RxStore<T> {
+    public root?: AbstractControl<T>;
+
+    public abstract get status(): Status;
+    public abstract get valid(): boolean;
+    public abstract get invalid(): boolean;
+    public abstract get pending(): boolean;
+    public abstract get touched(): boolean;
+    public abstract get untouched(): boolean;
+    public abstract get dirty(): boolean;
+    public abstract get pristine(): boolean;
+
+    public abstract markAsTouched(): void;
+    public abstract markAsDirty(): void;
+    public abstract validate(): void;
+}
+
+export { AbstractControl as default, Status };
